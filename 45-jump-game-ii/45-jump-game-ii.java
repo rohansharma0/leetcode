12fbaa1a13b0c753d85c42ca260cc37fc86dc2a4
Rohan Sharma <61389059+rohansharma0@users.noreply.github.com>
2022-03-11 10:28:58 +0530

    Time: 91 ms (23.82%), Space: 49.8 MB (18.20%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,31 +1,46 @@
 class Solution {
     public int jump(int[] nums) {
         
-        int[] dp = new int[nums.length];
+        int n = nums.length;
         
-        Arrays.fill(dp , -1);
+        int[] dp = new int[n];
         
-        return solve(0,nums , dp);
+        for(int i = n-2 ; i>= 0 ; i--){
+            int min = (int)1e8;
+        
+            for(int j = 1 ; j<=nums[i] ; j++){
+                
+                int d = 0;
+                if(i + j < n){
+                    d = dp[i+j];
+                }
+                min = Math.min(min , d);
+            }
+            dp[i] = min + 1;
+        }
+        
+        
+        return dp[0];
        
     }
     
-    public int solve(int idx , int[] nums , int[] dp){
+//     public int solve(int idx , int[] nums , int[] dp){
         
-        if(idx == nums.length -1) return 0;
+//         if(idx == nums.length -1) return 0;
         
-        if(idx > nums.length -1) return (int)1e8;
+//         if(idx > nums.length -1) return (int)1e8;
         
-        if(dp[idx] != -1) return dp[idx];
+//         if(dp[idx] != -1) return dp[idx];
         
-        int min = (int)1e8;
+//         int min = (int)1e8;
         
-        for(int i = 1 ; i<=nums[idx] ; i++){
-            int d = solve(idx + i , nums , dp);
+//         for(int i = 1 ; i<=nums[idx] ; i++){
+//             int d = solve(idx + i , nums , dp);
             
-            min = Math.min(min , d);
-        }
+//             min = Math.min(min , d);
+//         }
         
-        return dp[idx] =  min + 1;
+//         return dp[idx] =  min + 1;
         
-    }
+//     }
 }
