e700f3b3d8cecded5300f99f5c7712b1a08a94a0
Rohan Sharma <61389059+rohansharma0@users.noreply.github.com>
2022-03-11 18:49:37 +0530

    Time: 2 ms (88.44%), Space: 68.3 MB (26.97%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,32 +1,36 @@
 class Solution {
     public boolean canJump(int[] nums) {
         
-        int[] dp = new int[nums.length];
+        int n = nums.length;
         
-        Arrays.fill(dp , -1);
-        //-1 for not visited
-        // 0 for visited but false
-        // 1 for visited but true 
+        int reachable = 0;
         
-        return solve(0 , nums , dp);
+        
+        for(int i = 0; i< n ; i++){
+            if(reachable < i) return false;
+            
+            reachable = Math.max(reachable , i + nums[i]);
+        }
+        
+        return true;
         
     }
     
-    public boolean solve(int idx , int[] nums , int[] dp){
+//     public boolean solve(int idx , int[] nums , int[] dp){
         
-        if(idx == nums.length - 1) return true;
-        if(idx > nums.length - 1) return false;
+//         if(idx == nums.length - 1) return true;
+//         if(idx > nums.length - 1) return false;
         
-        if(dp[idx] != -1) return dp[idx] == 1 ? true : false; 
+//         if(dp[idx] != -1) return dp[idx] == 1 ? true : false; 
         
-        for(int i = 1 ; i<= nums[idx] ; i++){
+//         for(int i = 1 ; i<= nums[idx] ; i++){
             
-            if(solve(idx+i , nums , dp)){
-                dp[idx + i] = 1; 
-                return true;
-            }
-        }
-        dp[idx] = 0;
-        return false;
-    }
+//             if(solve(idx+i , nums , dp)){
+//                 dp[idx + i] = 1; 
+//                 return true;
+//             }
+//         }
+//         dp[idx] = 0;
+//         return false;
+//     }
 }
