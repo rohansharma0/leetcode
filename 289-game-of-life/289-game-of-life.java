20128ab9def701d52d60bedc1d7321be31d1b0e1
Rohan Sharma <61389059+rohansharma0@users.noreply.github.com>
2022-03-13 13:37:41 +0530

    Time: 1 ms (57.80%), Space: 42 MB (53.37%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.java
@@ -0,0 +1,93 @@
+class Solution {
+    public void gameOfLife(int[][] board) {
+        
+        int n = board.length;
+        int m = board[0].length;
+        
+        for(int i = 0 ; i< n ; i++){
+            
+            for(int j = 0; j< m ; j++){
+                
+                if(board[i][j] == 0){
+                    solve(board , i , j);
+                }else{
+                    solve(board , i , j);
+                }
+                
+            }
+        }
+        
+        for(int i = 0 ; i< n ; i++){
+            
+            for(int j = 0; j< m ; j++){
+                
+                if(board[i][j] == -1){
+                    board[i][j] = 0;
+                }else if(board[i][j] == -2){
+                    board[i][j] = 1;
+                }
+                
+            }
+        }
+        
+    }
+    
+    public void solve(int[][] board , int i , int j){
+        
+        int n = board.length;
+        int m = board[0].length;
+        
+        int n1 = 0;
+        int cnt = 0;
+        
+        if(i-1 >= 0){
+            cnt++;
+            if(Math.abs(board[i-1][j]) == 1) n1++;
+        }
+        if(i-1 >= 0 && j+1 < m){
+            cnt++;
+            if(Math.abs(board[i-1][j+1]) == 1) n1++;
+        }
+        if(i+1 < n){
+            cnt++;
+            if(Math.abs(board[i+1][j]) == 1) n1++;
+        }
+        if(i+1 <n && j+1 < m){
+            cnt++;
+            if(Math.abs(board[i+1][j+1]) == 1) n1++;
+        }
+        if(j+1 < m){
+            cnt++;
+            if(Math.abs(board[i][j+1]) == 1) n1++;
+        }
+        if(i+1 <n  && j-1 >=0){
+            cnt++;
+            if(Math.abs(board[i+1][j-1]) == 1) n1++;
+        }
+        if(j-1 >= 0){
+            cnt++;
+            if(Math.abs(board[i][j-1]) == 1) n1++;
+        }
+        if(i-1 >= 0 && j-1 >=0){
+            cnt++;
+            if(Math.abs(board[i-1][j-1]) == 1) n1++;
+        }
+        
+        int n0 = cnt - n1;
+        
+        if(board[i][j] == 1){
+            
+            if(n1 < 2 || n1 > 3){
+                //0
+                board[i][j] = -1;
+            }
+        }else{
+            
+            if(n1 == 3){
+                //1
+                board[i][j] = -2;
+            }
+            
+        }
+    }
+}
