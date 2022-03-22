c6a13ce6cc8121ade6a595a0b33d6558cc52a0d4
Rohan Sharma <61389059+rohansharma0@users.noreply.github.com>
2022-03-22 16:16:00 +0530

    Time: 2 ms (91.34%), Space: 43.6 MB (20.17%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.java
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -4,27 +4,47 @@ class Solution {
         int n = s.length();
         int m = p.length();
         
-        return solve(n-1 , m-1 , s, p);
+        int[][] dp = new int[n+1][m+1];
+        
+        for(int[]x : dp ){
+            Arrays.fill(x , -1);
+        }
+        return solve(n , m , s, p , dp);
     }
-    public boolean solve(int i , int j , String s , String p){
+    public boolean solve(int i , int j , String s , String p , int[][] dp){
         
-        if(j<0) return (i<0);
+        if(j==0) return (i==0);
         
-        if(i<0) return (p.charAt(j) == '*') && solve(i,j-2,s,p);
-        
-        
-        
-        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') return solve(i-1 , j-1 , s , p);
-        
-        
-        if(p.charAt(j) == '*'){
-
-            if(s.charAt(i) == p.charAt(j-1) || p.charAt(j-1) == '.') return solve(i-1 , j , s , p) || solve(i , j-2 , s ,p);
+        if(i==0) return p.charAt(j-1) == '*' && solve(i,j-2,s, p , dp);
             
-            return solve(i , j-2 , s ,p);
+        if(dp[i][j] != -1) return (dp[i][j] == 1);
+        
+        boolean ans = false;
+        
+        //simple char matching
+        //if s char matchs with p char or it can be '.'
+        if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.'){
+            ans = solve(i-1 , j-1 , s , p , dp);
         }
         
-        return false;
+        //if char is '*' 
+        else if(p.charAt(j-1) == '*'){
+            
+            //if s char matchs with p char or it can be '.'
+            if(s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.'){
+                
+                //using * we can take one char or we can ignore *
+                ans = ans || solve(i-1 , j , s , p , dp);
+
+            }
+            
+            // if s char not match with p char
+            ans = ans || solve(i , j-2 , s , p , dp);
+           
+        }
         
+        dp[i][j] = ans ? 1 : 0;
+        
+        return ans;
     }
 }
