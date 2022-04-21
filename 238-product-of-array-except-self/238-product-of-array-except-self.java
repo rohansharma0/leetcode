4fdc825bbc1bce8a135b38adcb7cff5e0098f0e1
Rohan Sharma <61389059+rohansharma0@users.noreply.github.com>
2022-04-21 21:16:02 +0530

    Time: 2 ms (77.68%), Space: 57.2 MB (71.61%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,29 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        
         int n = nums.length;
-        int[] res = new int[n];
         
-        //left
-        int left = 1;
-        for(int i = 0; i< n ; i++){
-            if(i > 0){
-                left = left * nums[i-1];
-            }
-            
-            res[i] = left;
-            
+        int[] ans = new int[n];
+        int[] lp = new int[n];
+        int[] rp = new int[n];
+        
+        lp[0] = nums[0];
+        for(int i = 1 ; i< n ; i++){
+            lp[i] = nums[i] * lp[i-1];
         }
         
-        //right
-        int right = 1;
-        for(int i = n-1 ; i>= 0 ; i--){
-            if(i < n-1){
-                right = right * nums[i+1];
-            }
-            res[i] = res[i] * right;
+        rp[n-1] = nums[n-1];
+        for(int i = n-2 ; i>= 0 ; i--){
+            rp[i] = nums[i] * rp[i+1];
         }
-        
-        return res;
+        ans[0] = rp[1];
+        ans[n-1] = lp[n-2];
+        for(int i = 1; i< nums.length-1 ; i++){
+            ans[i] = lp[i-1];
+            ans[i] = ans[i] * rp[i+1];
+        }
+        return ans;
     }
 }
