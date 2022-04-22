8b59a8fcb78f2b605912d79293930bfaf1707293
Rohan Sharma <61389059+rohansharma0@users.noreply.github.com>
2022-04-22 14:46:56 +0530

    Time: 5 ms (36.89%), Space: 68.9 MB (15.79%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        
         int n = nums.length;
         
         int reachable = 0;
-        
         
         for(int i = 0; i< n ; i++){
             if(reachable < i) return false;
@@ -13,24 +11,6 @@ class Solution {
         }
         
         return true;
-        
     }
     
-//     public boolean solve(int idx , int[] nums , int[] dp){
-        
-//         if(idx == nums.length - 1) return true;
-//         if(idx > nums.length - 1) return false;
-        
-//         if(dp[idx] != -1) return dp[idx] == 1 ? true : false; 
-        
-//         for(int i = 1 ; i<= nums[idx] ; i++){
-            
-//             if(solve(idx+i , nums , dp)){
-//                 dp[idx + i] = 1; 
-//                 return true;
-//             }
-//         }
-//         dp[idx] = 0;
-//         return false;
-//     }
 }
