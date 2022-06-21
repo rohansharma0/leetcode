6c24ab70c146d53a67a2b94108cf1adeb7f1e045
Rohan Sharma <61389059+rohansharma0@users.noreply.github.com>
2022-06-21 18:59:41 +0530

    Time: 2 ms (83.08%), Space: 44.2 MB (39.05%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,30 +1,27 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        
         List<List<Integer>> ans = new ArrayList<>();
-        
         Arrays.sort(nums);
         
-        getSubsets(0 , nums , new ArrayList<>() , ans);
+        solve(0 , nums , new ArrayList<>() , ans);
         
         return ans;
-      
     }
     
-    public void getSubsets(int idx , int[] nums , List<Integer> ds , List<List<Integer>> ans){
+    public void solve(int i , int[] nums , List<Integer> ds , List<List<Integer>> ans){
         
-        ans.add(new ArrayList<>(ds));
-        
-        for(int i = idx; i< nums.length ; i++){
-            
-            if(i != idx && nums[i] == nums[i-1]) continue;
-            
-            ds.add(nums[i]);
-            getSubsets(i + 1 , nums , ds , ans);
-            ds.remove(ds.size() -1);
-            
+        if(i == nums.length){
+            ans.add(new ArrayList<>(ds));
+            return;
         }
         
+        //take
+        ds.add(nums[i]);
+        solve(i+1 , nums , ds , ans);
+        ds.remove(ds.size()-1);
+        
+        //notTake
+        while(i+1 < nums.length && nums[i] == nums[i+1]) i++;
+        solve(i+1 , nums , ds , ans);
     }
-   
 }
