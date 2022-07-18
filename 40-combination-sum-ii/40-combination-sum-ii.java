b3ea056db05d7237c192097bd69cc71dae73c71d
Rohan Sharma <61389059+rohansharma0@users.noreply.github.com>
2022-07-19 01:15:14 +0530

    Time: 4 ms (89.49%), Space: 44.5 MB (18.67%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,38 +1,34 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+       
         List<List<Integer>> ans = new ArrayList<>();
-        
         Arrays.sort(candidates);
+        List<Integer> ds = new ArrayList<>();
         
-        solve(0 , target , candidates , new ArrayList<>() , ans);
+        solve(0 , ds, candidates , target , ans);
         
         return ans;
     }
     
-    public void solve(int i , int tar , int[] nums , List<Integer> ds , List<List<Integer>> ans){
-        
+    
+    public void solve(int idx , List<Integer> ds , int[] nums , int tar , List<List<Integer>> ans ){
         
         if(tar == 0){
             ans.add(new ArrayList<>(ds));
             return;
         }
         
-        
-        if(i >= nums.length) return;
-        
-        
-        //take
-        if(nums[i] <= tar){
+        for(int i = idx ; i < nums.length ; i++){
+                        
+            if(i != idx && nums[i] == nums[i-1]) continue;
+
+            if(nums[i] > tar) break;
+            
             ds.add(nums[i]);
-            solve(i+1, tar - nums[i] , nums , ds , ans);
+            solve(i+1 , ds , nums , tar - nums[i] , ans);
             ds.remove(ds.size()-1);
-        
+
         }
-        
-        while(i+1 < nums.length && nums[i] == nums[i+1]) i++;
-        
-        //notTake
-        solve(i+1 , tar , nums , ds , ans);
         
     }
 }
