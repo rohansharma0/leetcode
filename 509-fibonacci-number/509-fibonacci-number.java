d8049933635157ad96ecda9757a4d92a38b3bfa2
Rohan Sharma <61389059+rohansharma0@users.noreply.github.com>
2022-07-31 00:16:06 +0530

    Time: 0 ms (100.00%), Space: 41 MB (39.68%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,14 +1,16 @@
 class Solution {
     public int fib(int n) {
-        if(n == 0) return 0;
-        int prev = 1;
+        
+        if(n <= 1) return n;
+        
         int prev2 = 0;
+        int prev = 1;
         
         for(int i = 2 ; i<= n ; i++){
-            int x = prev2 + prev;
-            
+            int curr = prev + prev2;
+
             prev2 = prev;
-            prev = x;
+            prev = curr;
         }
         
         return prev;
