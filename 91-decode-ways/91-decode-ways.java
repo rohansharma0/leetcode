09ea06ca4e0ba59d4e11a93c5ddf118068b2badd
Rohan Sharma <61389059+rohansharma0@users.noreply.github.com>
2022-08-06 18:23:00 +0530

    Time: 1 ms (98.76%), Space: 42.5 MB (39.19%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,29 +1,39 @@
 class Solution {
     public int numDecodings(String s) {
+        
         int n = s.length();
-        int[] dp = new int[n+1];
         
-        dp[n] = 1;
+        int[] dp = new int[n];
         
-        for(int i = n-1 ; i>= 0 ; i--){
-            
-            int l = 0;
-            if(s.charAt(i)-'0' > 0){
-                l = dp[i+1];
-            }
-            int r = 0;
-            if(i < n-1 && s.charAt(i) != '0'){
-                int x = (s.charAt(i)-'0') * 10 + (s.charAt(i+1)-'0');
-
-                if(x > 0 && x <= 26){
-                    r = dp[i+2];
-                }
-            }
-
-            dp[i] = l + r;
-            
-        }
-        return dp[0];
+        Arrays.fill(dp , -1);
+        
+        return solve(0 , s , dp);
+        
     }
     
+    public int solve(int i  , String s , int[] dp){
+        
+        if(i == s.length()) return 1;
+        
+        if(dp[i] != -1) return dp[i];
+        
+        int l = 0 , r = 0;
+        
+        if(s.charAt(i) - '0' > 0){
+            l = solve(i+1 , s , dp);
+        }
+        
+        if(i+1 < s.length() && s.charAt(i) != '0'){
+            
+            int x = (s.charAt(i) - '0') * 10 +( s.charAt(i+1) - '0');
+            
+            if(x > 0 && x <= 26){
+                r = solve(i+2 , s , dp);
+            }
+           
+        }
+        
+        return dp[i] = l + r;
+        
+    }
 }
