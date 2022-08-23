c675172c9d654a1acd231105afda73944a8d3a74
Rohan Sharma <61389059+rohansharma0@users.noreply.github.com>
2022-08-23 17:32:08 +0530

    Time: 10 ms (42.12%), Space: 44.6 MB (73.32%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -4,44 +4,33 @@ class Solution {
         int n = s.length();
         int m = t.length();
         
-        int[][] dp = new int[n+1][m+1];
-        
-        for(int j = 0; j<= m ; j++) dp[0][j] = j;
-        for(int i = 0; i<= n ; i++) dp[i][0] = i;
+        int[]prev = new int[m+1];
         
         
-        for(int i = 1 ; i<= n ; i++){
+        for(int j = 0 ; j<=m ; j++){
+            prev[j] = j;
+        }
+        
+        for(int i = 1; i<=n ; i++){
             
-            for(int j = 1 ; j<= m ; j++){
+            int[] curr = new int[m+1];
+            curr[0] = i;
+            
+            for(int j = 1 ; j<= m ;j++){
                 
                 if(s.charAt(i-1) == t.charAt(j-1)){
-                    
-                    dp[i][j] = dp[i-1][j-1];
-                    
+                    curr[j] = prev[j-1];
                 }else{
-                    
-                    dp[i][j] = Math.min(Math.min(dp[i][j-1] , dp[i-1][j]) , dp[i-1][j-1]) + 1;
-                    
+                    curr[j] = Math.min(prev[j-1] , Math.min(curr[j-1] , prev[j])) + 1;
                 }
-               
+                
             }
+            
+            prev = curr;
         }
         
         
-        return dp[n][m];
-    }
-    
-    public int solve(int i , int j , String s , String t , int[][] dp){
-        
-        if(i < 0) return j+1;
-        if(j < 0) return i+1;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        if(s.charAt(i) == t.charAt(j)) return solve(i -1 , j-1 , s ,t , dp);
-        
-        return 1 + Math.min(Math.min(solve(i , j-1 , s ,t, dp) , solve(i-1 , j , s ,t , dp)) , solve(i-1 , j-1 , s ,t , dp));
+        return prev[m];
         
     }
-    
 }
