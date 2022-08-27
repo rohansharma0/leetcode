ba64484aa05c4ace6d3f52a0f7f719aec37a75f7
Rohan Sharma <61389059+rohansharma0@users.noreply.github.com>
2022-08-27 13:18:58 +0530

    Time: 105 ms (19.20%), Space: 44.8 MB (79.91%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -3,43 +3,51 @@ class Solution {
         
         int n = words.length;
         
-        Arrays.sort(words , (a , b) -> a.length() - b.length());
-        
         int[] dp = new int[n];
-        
+        Arrays.sort(words , (a , b) -> a.length() - b.length());
         Arrays.fill(dp , 1);
+        
         int max = 1;
         
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j< i ; j++){
-                if(check(words[i] , words[j]) && dp[j] + 1 > dp[i]){
-                    dp[i] = dp[j] +1;
+        for(int i = 0 ; i< n ; i++){
+            
+            
+            for(int j = 0 ; j < i ; j++){
+                
+                if(compare(words[i] , words[j]) && dp[i] < dp[j] + 1){
+                    
+                    dp[i] = dp[j] + 1;
+                    
                 }
+                
             }
-            max  = Math.max(max , dp[i]);
+            
+            max = Math.max(max , dp[i]);
+            
         }
         
-        
         return max;
-        
     }
     
-    public boolean check(String a , String b){
+    public boolean compare(String s , String t){
         
-        if(a.length() != b.length() + 1) return false;
+        if(s.length() != t.length() + 1) return false;
+        
         int i = 0;
         int j = 0;
         
-        
-        while(i < a.length()){
-            if( j < b.length() && a.charAt(i) == b.charAt(j)){
+        while(i < s.length()){
+            
+            if(j < t.length() && s.charAt(i) == t.charAt(j)){
                 i++;
                 j++;
             }else{
                 i++;
             }
+            
         }
-        if(i == a.length() && j == b.length()) return true;
+        
+        if(i == s.length() && j == t.length()) return true;
         return false;
     }
 }
