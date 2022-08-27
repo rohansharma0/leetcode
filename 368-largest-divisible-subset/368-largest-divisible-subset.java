bb93ea32d1c98e8f3f56055696a11a9d5b5366b1
Rohan Sharma <61389059+rohansharma0@users.noreply.github.com>
2022-08-27 12:42:16 +0530

    Time: 40 ms (26.70%), Space: 44.1 MB (27.17%) - LeetHub

--- 368-largest-divisible-subset/368-largest-divisible-subset.java
+++ 368-largest-divisible-subset/368-largest-divisible-subset.java
@@ -1,41 +1,47 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
+        
+        Arrays.sort(nums);
+        
         int n = nums.length;
         
         int[] dp = new int[n];
         int[] hash = new int[n];
+        
         Arrays.fill(dp , 1);
         
-        Arrays.sort(nums);
+        int max = 1;
+        int lastI = 0;
         
-        int maxi = 1;
-        int lastIndex = 0;
-        for(int i = 0; i< n ; i++){
+        for(int i = 0 ; i< n ; i++){
+            
             hash[i] = i;
-            for(int j = 0 ; j < i ; j++){
+            
+            for(int j = 0 ; j < i  ; j++){
                 
                 if(nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1){
-                    dp[i] = 1 + dp[j];
+                    dp[i] = dp[j] + 1;
                     hash[i] = j;
                 }
+                
             }
-            if(dp[i] > maxi){
-                maxi = dp[i];
-                lastIndex = i;
+            
+            if(dp[i] > max){
+                max = dp[i];
+                lastI = i;
             }
+            
         }
         
-        // printing sequence
-        List<Integer> arr = new ArrayList<>();
-        arr.add(nums[lastIndex]);
-        while(hash[lastIndex] != lastIndex){
-            lastIndex = hash[lastIndex];
-            arr.add(nums[lastIndex]);
+        List<Integer> ans = new ArrayList<>();
+        ans.add(nums[lastI]);
+        
+        while(hash[lastI] != lastI){
+            lastI = hash[lastI];
+            ans.add(nums[lastI]);
         }
         
-        Collections.reverse(arr);
-
-        
-        return arr;
+        Collections.reverse(ans);
+        return ans;
     }
 }
